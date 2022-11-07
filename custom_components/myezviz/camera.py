da60a1ee94dfaeb6781cff94a71c0978d6781317
c1pher <c1pher-cn@users.noreply.github.com>
2022-11-08 00:27:24 +0800

    Update camera.py
    支持猫眼

--- custom_components/myezviz/camera.py
+++ custom_components/myezviz/camera.py
@@ -5,7 +5,8 @@ https://home-assistant.io/components/demo/
 """
 import os
 import time
-from datetime import timedelta
+import datetime
+#from datetime import timedelta
 import requests
 import logging
 
@@ -24,11 +25,13 @@ CONF_ID = 'id'
 CONF_KEY = 'key'
 CONF_SEC = 'sec'
 CONF_NAME = 'name'
+CONF_MAOYAN = 'ismaoyan'
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_ID): cv.string,
     vol.Required(CONF_KEY): cv.string,
     vol.Required(CONF_SEC): cv.string,
     vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_MAOYAN): cv.string,
 })
 
 
@@ -48,31 +51,25 @@ class EzvizCamera(Camera):
         self._parent = hass
         self._name = config.get(CONF_NAME)
         self._motion_status = False
+        self._isMaoyan = 'notmaoyan'
+        _LOGGER.error("initconfig")
+        self._isMaoyan = config.get(CONF_MAOYAN)
+        _LOGGER.error('%s' % self._isMaoyan)
         self.appKey = config.get(CONF_KEY)
         self.appSecret = config.get(CONF_SEC)
         self.accessToken = ""
         self.deviceSerial = config.get(CONF_ID)
-        self.expireTime = 0 
+        self.expireTime = 0
 
         self._is_streaming = None
         self._is_video_history_enabled = False
-        
+
         # Default to non-NestAware subscribed, but will be fixed during update
-        self._time_between_snapshots = timedelta(seconds=30)
+        self._time_between_snapshots = datetime.timedelta(seconds=30)
         self._last_image = None
         self._next_snapshot_at = None
+        self._attr_unique_id = self._name + self.deviceSerial
 
-    @property
-    def name(self):
-        """Return the name of this camera."""
-        return self._name
-
-    @property
-    def should_poll(self):
-        """Camera should poll periodically."""
-        return True
-
-    @property
 #    def motion_detection_enabled(self):
 #        r = requests.post('https://open.ys7.com/api/lapp/device/info', data={'accessToken':self.accessToken,'deviceSerial':self.deviceSerial})
 #        result = r.json()
@@ -102,42 +99,81 @@ class EzvizCamera(Camera):
         else:
             return False
 
-    def get_device_capture(deviceSerial):
-        r = requests.post('https://open.ys7.com/api/lapp/device/capture', data={'accessToken':self.accessToken,'deviceSerial':self.deviceSerial,'channelNo':1})
+    def get_device_capture(self):
+        r = requests.post('https://open.ys7.com/api/lapp/device/capture', data={'accessToken':self.accessToken,'deviceSerial':self.deviceSerial,'channelNo':1,'quality':1})
         result = r.json()
         if (result['code']=='200'):
             return result['data']['picUrl']
         else:
-            return ''
+            return 'error'
+
+    def get_device_message(self):
+        r = requests.post('https://open.ys7.com/api/lapp/alarm/device/list', data={'accessToken':self.accessToken,'deviceSerial':self.deviceSerial})
+        result = r.json()
+        if (result['code']=='200'):
+            if len(result['data']) > 0:
+                #_LOGGER.error(result)
+                alarmType = result['data'][0]['alarmType']
+                alarmTime = result['data'][0]['alarmTime']
+                alarmPicUrl = result['data'][0]['alarmPicUrl']
+                strtime = datetime.datetime.fromtimestamp(alarmTime/1000)
+                return alarmPicUrl
+            else:
+                return 'error'
+        else:
+            return 'error'
+
+
     def _ready_for_snapshot(self, now):
         return (self._next_snapshot_at is None or
-                now > self._next_snapshot_at)  
-  
+                now > self._next_snapshot_at)
+
     def camera_image(
-              self, width: int , height: int 
+              self, width: int , height: int
     ):
         """Return a faked still image response."""
+        #_LOGGER.error("camera_image")
         now = dt_util.utcnow()
         if self._ready_for_snapshot(now):
-        
+            #_LOGGER.error("self._ready_for_snapshot")
+
             nowss = int(round(time.time() * 1000))
 
             if (nowss > (self.expireTime-1000)):
+                #_LOGGER.error("nowss > (self.expireTime-1000)")
                 r = requests.post('https://open.ys7.com/api/lapp/token/get', data={'appKey':self.appKey,'appSecret':self.appSecret})
                 token_result = r.json()
                 if (token_result['code']=='200'):
                     self.accessToken = token_result['data']['accessToken']
                     self.expireTime = token_result['data']['expireTime']
+                    #_LOGGER.error("get token success")
                 else:
-                    _LOGGER.info("get token error")
-            r = requests.post('https://open.ys7.com/api/lapp/device/capture', data={'accessToken':self.accessToken,'deviceSerial':self.deviceSerial,'channelNo':1})
-            result = r.json()
-            if (result['code']=='200'):
-                image_path = result['data']['picUrl']
+                    _LOGGER.error("get token error")
+
+
+            #r = requests.post('https://open.ys7.com/api/lapp/device/capture', data={'accessToken':self.accessToken,'deviceSerial':self.deviceSerial,'channelNo':1,'quality':1})
+            #result = r.json()
+            #if (result['code']=='200'):
+            #    image_path = result['data']['picUrl']
+            #else:
+            #    _LOGGER.info("get image error")
+            #    image_path = 'null'
+            #    return None
+            _LOGGER.error(self._isMaoyan)
+            _LOGGER.error('%s' % self._isMaoyan)
+            if self._isMaoyan == 'maoyan' :
+                _LOGGER.error("is MaoYan")
+                _LOGGER.error(self.deviceSerial)
+                image_path = self.get_device_message()
             else:
-                _LOGGER.info("get image error")
-                image_path = 'null'
+                _LOGGER.error("not MaoYan")
+                _LOGGER.error(self.deviceSerial)
+                image_path = self.get_device_capture()
+            #_LOGGER.error("Get camera image: %s" % image_path)
+            #_LOGGER.error("Config: %s" % self._isMaoyan)
+            if image_path == 'error':
                 return None
+
             try:
                 response = requests.get(image_path)
             except requests.exceptions.RequestException as error:
@@ -148,3 +184,13 @@ class EzvizCamera(Camera):
             self._last_image = response.content
 
         return self._last_image
+
+    @property
+    def name(self):
+        """Return the name of this camera."""
+        return self._name
+
+    @property
+    def should_poll(self):
+        """Camera should poll periodically."""
+        return True
